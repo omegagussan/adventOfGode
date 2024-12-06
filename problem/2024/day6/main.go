9cb5eae9a7b6aaa605ec12f4a729f5198ebf7269
Gustav Sandström <gustav.sandstrom@epidemicsound.com>
2024-12-06 14:08:35 +0100

    tidy

--- problem/2024/day6/main.go
+++ problem/2024/day6/main.go
@@ -7,8 +7,12 @@ import (
 )
 
 type Point struct {
-	x int
-	y int
+	x, y int
+}
+
+type State struct {
+	p Point
+	d int
 }
 
 var directions = []Point{{0, -1}, {1, 0}, {0, 1}, {-1, 0}}
@@ -18,10 +22,9 @@ func main() {
 	bytes, _ := os.ReadFile(dir + "/problem/2024/day6/input.txt")
 	input := string(bytes)
 	m := parseMap(input)
-	guard := findStartGuard(m)
-	fmt.Println(part1(m, Point{guard.x, guard.y}))
-	fmt.Println(part2(m, Point{guard.x, guard.y}))
-
+	visited, _ := part1(m, findStartGuard(m))
+	fmt.Println(countUniquePose(visited) - 1)
+	fmt.Println(part2(m, findStartGuard(m), visited))
 }
 
 func parseMap(input string) []string {
@@ -30,69 +33,51 @@ func parseMap(input string) []string {
 
 func findStartGuard(input []string) Point {
 	for i, v := range input {
-		index := strings.Index(v, "^")
-		if index != -1 {
+		if index := strings.Index(v, "^"); index != -1 {
 			return Point{index, i}
 		}
 	}
 	return Point{}
 }
 
-func part1(mapz []string, guard Point) int {
+func part1(mapz []string, guard Point) (map[State]struct{}, bool) {
 	direction := 0
-	visited := make(map[Point][]int)
-	visited[guard] = append(visited[guard], direction)
+	visitedSet := make(map[State]struct{})
 	for isInMap(mapz, guard) {
 		guard, direction = move(mapz, guard, direction)
-		visited[guard] = append(visited[guard], direction)
-
-		//inf loop protection
-		guardVals := visited[guard]
-		if findAnyDuplicate(guardVals) != -1 {
-			return -1
+		if _, exists := visitedSet[State{guard, direction}]; exists {
+			return visitedSet, true
 		}
+		visitedSet[State{guard, direction}] = struct{}{}
 	}
-	return len(visited) - 1
+	return visitedSet, false
 }
 
-func part2(mapz []string, guard Point) int {
-
-	oldGuard := Point{guard.x, guard.y}
-	direction := 0
-	visited := make(map[Point][]int)
-	visited[guard] = append(visited[guard], direction)
-	for isInMap(mapz, guard) {
-		guard, direction = move(mapz, guard, direction)
-		visited[guard] = append(visited[guard], direction)
-	}
-
+func part2(mapz []string, guard Point, visited map[State]struct{}) int {
 	infLoops := make(map[Point]bool)
-	for v, _ := range visited {
-		if !isInMap(mapz, v) {
+	for v := range visited {
+		if infLoops[v.p] || !isInMap(mapz, v.p) || mapz[v.p.y][v.p.x] == '#' {
 			continue
 		}
-		if mapz[v.y][v.x] == '#' {
-			continue
-		}
-		tmpMap := copyMap(mapz)
-		insertBarrel(tmpMap, v)
-		if part1(tmpMap, Point{x: oldGuard.x, y: oldGuard.y}) == -1 {
-			infLoops[v] = true
+		_, b := part1(insertBarrel(copyMap(mapz), v.p), guard)
+		if b {
+			infLoops[v.p] = true
 		}
 	}
 	return len(infLoops)
 }
 
 func copyMap(mapz []string) []string {
-	var tmpMap []string
-	for _, v := range mapz {
-		tmpMap = append(tmpMap, strings.Clone(v))
+	tmpMap := make([]string, len(mapz))
+	for i, v := range mapz {
+		tmpMap[i] = strings.Clone(v)
 	}
 	return tmpMap
 }
 
-func insertBarrel(tmpMap []string, n Point) {
+func insertBarrel(tmpMap []string, n Point) []string {
 	tmpMap[n.y] = replaceAtIndex(tmpMap[n.y], '#', n.x)
+	return tmpMap
 }
 
 func replaceAtIndex(in string, r rune, i int) string {
@@ -107,32 +92,26 @@ func isInMap(input []string, p Point) bool {
 
 func move(mapz []string, p Point, d int) (Point, int) {
 	tmp := addPoint(p, directions[d])
+
+	//look before you leap
 	if !isInMap(mapz, tmp) {
 		return tmp, d
 	}
-	mapVal := string(mapz[tmp.y][tmp.x])
-	if mapVal != "#" {
-		p = tmp
-		return p, d
-	} else {
-		d = (d + 1) % 4
-		return p, d
+	if mapz[tmp.y][tmp.x] != '#' {
+		return tmp, d
 	}
+
+	return p, (d + 1) % 4
 }
 
-func addPoint(p1 Point, p2 Point) Point {
+func addPoint(p1, p2 Point) Point {
 	return Point{p1.x + p2.x, p1.y + p2.y}
 }
 
-//1587 => high
-
-func findAnyDuplicate(input []int) int {
-	seen := make(map[int]bool)
-	for _, v := range input {
-		if seen[v] {
-			return v
-		}
-		seen[v] = true
+func countUniquePose(state map[State]struct{}) int {
+	tmp := make(map[Point]struct{})
+	for k := range state {
+		tmp[k.p] = struct{}{}
 	}
-	return -1
+	return len(tmp)
 }
