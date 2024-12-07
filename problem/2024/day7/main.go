a465454ddb44cbfe1570f300685662479efd467c
Gustav Sandström <gustav.sandstrom@epidemicsound.com>
2024-12-07 11:10:32 +0100

    part2

--- problem/2024/day7/main.go
+++ problem/2024/day7/main.go
@@ -12,13 +12,14 @@ type Equation struct {
 	result int
 }
 
-func (e *Equation) numberSolutions() int {
+func (e *Equation) numberSolutions(withSecretElephant bool) int {
 	tmp := make([]int, len(e.terms))
 	copy(tmp, e.terms)
 
 	sums := make([]int, 0)
 	//pop
 	elem, tmp := tmp[0], tmp[1:]
+
 	sums = append(sums, elem)
 	for len(tmp) > 0 {
 		newSums := make([]int, 0)
@@ -28,6 +29,10 @@ func (e *Equation) numberSolutions() int {
 		for v := range sums {
 			newSums = append(newSums, sums[v]+elem)
 			newSums = append(newSums, sums[v]*elem)
+			if withSecretElephant {
+				concat, _ := strconv.Atoi(strconv.Itoa(sums[v]) + strconv.Itoa(elem))
+				newSums = append(newSums, concat)
+			}
 		}
 		sums = newSums
 	}
@@ -46,13 +51,26 @@ func main() {
 	bytes, _ := os.ReadFile(dir + "/problem/2024/day7/input.txt")
 	input := strings.Split(string(bytes), "\n")
 	fmt.Println(part1(input))
+	fmt.Println(part2(input))
+
 }
 
 func part1(input []string) int {
 	total := 0
 	for _, v := range input {
 		eq := parseEquation(v)
-		if eq.numberSolutions() > 0 {
+		if eq.numberSolutions(false) > 0 {
+			total += eq.result
+		}
+	}
+	return total
+}
+
+func part2(input []string) int {
+	total := 0
+	for _, v := range input {
+		eq := parseEquation(v)
+		if eq.numberSolutions(true) > 0 {
 			total += eq.result
 		}
 	}
@@ -63,7 +81,7 @@ func parseEquation(v string) Equation {
 	parts := strings.Split(v, ":")
 	target := parts[0]
 	result, _ := strconv.Atoi(target)
-	vals := strings.Split(strings.Join(parts[1:], ""), " ")
+	vals := strings.Split(strings.Trim(strings.Join(parts[1:], ""), " "), " ")
 	ints := make([]int, 0)
 	for _, val := range vals {
 		atoi, _ := strconv.Atoi(val)
