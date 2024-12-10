67d37636ba9310cf5a9ba327cf1c1a75cf1023f3
Gustav Sandström <gustav.sandstrom@epidemicsound.com>
2024-12-10 12:10:21 +0100

    tidy

--- problem/2024/day10/main.go
+++ problem/2024/day10/main.go
@@ -7,18 +7,11 @@ import (
 	"strings"
 )
 
-var dirs = []Point{
-	{1, 0, 0},
-	{0, 1, 0},
-	{-1, 0, 0},
-	{0, -1, 0},
+type Point struct {
+	y, x, val int
 }
 
-type Point struct {
-	y   int
-	x   int
-	val int
-}
+var dirs = []Point{{1, 0, 0}, {0, 1, 0}, {-1, 0, 0}, {0, -1, 0}}
 
 func (p Point) Add(p2 Point) Point {
 	return Point{p.y + p2.y, p.x + p2.x, p.val}
@@ -27,96 +20,71 @@ func (p Point) Add(p2 Point) Point {
 func (p Point) isInMap(topo []string) bool {
 	return p.y >= 0 && p.y < len(topo) && p.x >= 0 && p.x < len(topo[0])
 }
+
 func main() {
 	dir, _ := os.Getwd()
 	bytes, _ := os.ReadFile(dir + "/problem/2024/day10/input.txt")
-	input := string(bytes)
-	topo := strings.Split(input, "\n")
+	topo := strings.Split(string(bytes), "\n")
 	fmt.Println(part1(topo))
 	fmt.Println(part2(topo))
 }
 
-func contains(arr []Point, elem Point) bool {
-	for _, ph := range arr {
-		if ph == elem {
-			return true
-		}
-	}
-	return false
-}
-
 func part1(topo []string) int {
-	starts := make([]Point, 0)
-	for y, line := range topo {
-		for x, char := range line {
-			if char == '0' {
-				p := Point{y, x, 0}
-				starts = append(starts, p)
-			}
-		}
-	}
-
+	starts := findStarts(topo)
 	total := 0
 	for _, start := range starts {
-		heads := []Point{start}
-		seen := make(map[Point]bool)
-		for len(heads) > 0 {
-			curr := heads[0]
-			heads = heads[1:]
-			nextVal := strconv.Itoa(curr.val + 1)
-			for _, dir := range dirs {
-				next := curr.Add(dir)
-				if !next.isInMap(topo) {
-					continue
-				}
-				if curr.val == 8 && string(topo[next.y][next.x]) == "9" {
-					seen[next] = true
-				} else if string(topo[next.y][next.x]) == nextVal {
-					p := Point{next.y, next.x, curr.val + 1}
-					if !contains(heads, p) {
-						heads = append(heads, p)
-					}
-				}
-			}
-		}
-		total += len(seen)
+		total += explore(topo, start, true)
 	}
 	return total
 }
 
 func part2(topo []string) int {
-	starts := make([]Point, 0)
+	starts := findStarts(topo)
+	total := 0
+	for _, start := range starts {
+		total += explore(topo, start, false)
+	}
+	return total
+}
+
+func findStarts(topo []string) []Point {
+	var starts []Point
 	for y, line := range topo {
 		for x, char := range line {
 			if char == '0' {
-				p := Point{y, x, 0}
-				starts = append(starts, p)
+				starts = append(starts, Point{y, x, 0})
 			}
 		}
 	}
+	return starts
+}
 
-	total := 0
-	for _, start := range starts {
-		heads := []Point{start}
-		seen := 0
-		for len(heads) > 0 {
-			curr := heads[0]
-			heads = heads[1:]
-			nextVal := strconv.Itoa(curr.val + 1)
-			for _, dir := range dirs {
-				next := curr.Add(dir)
-				if !next.isInMap(topo) {
-					continue
+func explore(topo []string, start Point, useMap bool) int {
+	heads := []Point{start}
+	seen := make(map[Point]bool)
+	count := 0
+	for len(heads) > 0 {
+		curr := heads[0]
+		heads = heads[1:]
+		nextVal := strconv.Itoa(curr.val + 1)
+		for _, dir := range dirs {
+			next := curr.Add(dir)
+			if !next.isInMap(topo) {
+				continue
+			}
+			if curr.val == 8 && string(topo[next.y][next.x]) == "9" {
+				if useMap {
+					seen[next] = true
+				} else {
+					count++
 				}
-				if curr.val == 8 && string(topo[next.y][next.x]) == "9" {
-					seen += 1
-				} else if string(topo[next.y][next.x]) == nextVal {
-					p := Point{next.y, next.x, curr.val + 1}
-					heads = append(heads, p)
-				}
+			} else if string(topo[next.y][next.x]) == nextVal {
+				heads = append(heads, Point{next.y, next.x, curr.val + 1})
 			}
 		}
-		total += seen
 	}
-	return total
+	if useMap {
+		return len(seen)
+	}
+	return count
 }
