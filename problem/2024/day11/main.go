58bdcf2cd44d8cda45ce2564239769c38ca8d644
Gustav Sandström <gustav.sandstrom@epidemicsound.com>
2024-12-11 08:28:19 +0100

    recursion with memorization. only track lenght

--- problem/2024/day11/main.go
+++ problem/2024/day11/main.go
@@ -7,53 +7,66 @@ import (
 	"strings"
 )
 
+type Key struct {
+	elem   int
+	length int
+}
+
 func main() {
 	dir, _ := os.Getwd()
 	bytes, _ := os.ReadFile(dir + "/problem/2024/day11/input.txt")
 	input := string(bytes)
-	//fmt.Println(driver(input, 25))
 	fmt.Println(driver(input, 75))
 }
 
 func driver(input string, length int) int {
+	cache := make(map[Key]int)
 	state := parseInput(input)
-	for i := 0; i < length; i++ {
-		fmt.Println(i)
-		state = simulate(state)
+	sum := 0
+	for _, v := range state {
+		k := Key{v, length}
+		sum += simulate(k, cache)
 	}
-	return len(state)
+	return sum
 }
 
-func simulate(state []int) []int {
-	pow10Cache := make(map[int]int)
-	for i := 0; i < len(state); i++ {
-		v := state[i]
-		length := intLength(v)
-		if v == 0 {
-			state[i] = 1
-		} else if length%2 == 0 {
-			l2 := length / 2
-			state = insert(state, i+1, state[i]%getTenBaseRaisedTo(l2, pow10Cache))
-			state[i] = state[i] / getTenBaseRaisedTo(l2, pow10Cache)
-			i++
-		} else {
-			state[i] = v * 2024
-		}
+func simulate(k Key, cache map[Key]int) int {
+	if cached, ok := cache[k]; ok {
+		return cached
 	}
-	return state
+
+	if k.length == 0 {
+		return 1
+	}
+	if k.elem == 0 {
+		kt := Key{1, k.length - 1}
+		res := simulate(kt, cache)
+		cache[kt] = res
+		return res
+	}
+	ll := intLength(k.elem)
+	if ll%2 != 0 {
+		kt := Key{k.elem * 2024, k.length - 1}
+		res := simulate(kt, cache)
+		cache[kt] = res
+		return res
+	}
+	tenPow := getTenBaseRaisedTo(ll / 2)
+	firstHalfKey := Key{k.elem / tenPow, k.length - 1}
+	firstHalf := simulate(firstHalfKey, cache)
+	cache[firstHalfKey] = firstHalf
+	secondHalfKey := Key{k.elem % tenPow, k.length - 1}
+	secondHalf := simulate(secondHalfKey, cache)
+	cache[secondHalfKey] = secondHalf
+	return firstHalf + secondHalf
 }
 
-func getTenBaseRaisedTo(length int, pow10Cache map[int]int) int {
-	if val, exists := pow10Cache[length]; exists {
-		return val
-	}
-
+func getTenBaseRaisedTo(length int) int {
 	result := 1
 	for i := 0; i < length; i++ {
 		result *= 10
 	}
 
-	pow10Cache[length] = result
 	return result
 }
 
