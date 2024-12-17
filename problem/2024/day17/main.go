39ba41e9551fedc462a350ab1fb06725a3c43279
Gustav Sandström <gustav.sandstroem@gmail.com>
2024-12-17 08:34:16 +0100

    wip

--- problem/2024/day17/main.go
+++ problem/2024/day17/main.go
@@ -13,49 +13,65 @@ var re = regexp.MustCompile(`.*: (\d+)`)
 
 func main() {
 	dir, _ := os.Getwd()
-	bytes, _ := os.ReadFile(dir + "/problem/2024/day17/sample.txt")
+	bytes, _ := os.ReadFile(dir + "/problem/2024/day17/input.txt")
 	input := string(bytes)
 	fmt.Println(part1(input))
 }
 
 func part1(input string) string {
 	s, i := parse(input)
-	fmt.Println(i)
-	fmt.Println(s)
 	output := ""
 	p := 0
 Outer:
 	for p < len(i) {
 		v := i[p]
+		operand := i[p+1]
 		switch v {
-		case 0:
-			divisor := math.Pow(2, float64(v))
+		case 0: // adv
+			divisor := math.Pow(2, float64(getComboOperandValue(operand, s)))
 			res := int(float64(s[4]) / divisor)
 			s[4] = res
-		case 1:
-			s[5] = s[5] ^ v
-		case 2:
-			s[5] = s[5] % 8
-		case 3:
+		case 1: // bxl
+			s[5] = s[5] ^ operand
+		case 2: // bst
+			s[5] = getComboOperandValue(operand, s) % 8
+		case 3: // jnz
 			if s[4] != 0 {
-				if p != v {
-					p = v
-					continue Outer
-				}
+				p = operand
+				continue Outer
 			}
-		case 4:
+		case 4: // bxc
 			s[5] = s[5] ^ s[6]
-		case 5:
-			j := s[5] % 8
+		case 5: // out
+			j := getComboOperandValue(operand, s) % 8
 			output += strconv.Itoa(j)
-		case 6:
-			divisor := math.Pow(2, float64(s[6]))
+		case 6: // bdv
+			divisor := math.Pow(2, float64(getComboOperandValue(operand, s)))
 			res := int(float64(s[4]) / divisor)
 			s[5] = res
+		case 7: // cdv
+			divisor := math.Pow(2, float64(getComboOperandValue(operand, s)))
+			res := int(float64(s[4]) / divisor)
+			s[6] = res
 		}
 		p += 2
 	}
 	return output
+}
+
+func getComboOperandValue(operand int, s map[int]int) int {
+	switch operand {
+	case 0, 1, 2, 3:
+		return operand
+	case 4:
+		return s[4]
+	case 5:
+		return s[5]
+	case 6:
+		return s[6]
+	default:
+		panic("this should not happen!")
+	}
 }
 
 func parse(input string) (map[int]int, []int) {
@@ -82,3 +98,5 @@ func parse(input string) (map[int]int, []int) {
 
 	return state, instructions
 }
+
+//432645324
