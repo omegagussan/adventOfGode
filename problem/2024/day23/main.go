458111aa9fc0f11d03c357f85d5efc28947bb6d1
Gustav Sandström <gustav.sandstrom@epidemicsound.com>
2025-01-23 16:31:17 +0100

    part2: naive, not at all sure to work

--- problem/2024/day23/main.go
+++ problem/2024/day23/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"os"
+	"sort"
 	"strings"
 )
 
@@ -28,6 +29,52 @@ func main() {
 	input := string(bytes)
 	split := strings.Split(input, "\n")
 	fmt.Println(part1(split))
+	fmt.Println(part2(split))
+}
+
+func part2(input []string) string {
+	connections := make(map[string][]string)
+	for _, line := range input {
+		split := strings.Split(line, "-")
+		connections[split[0]] = append(connections[split[0]], split[1])
+		connections[split[1]] = append(connections[split[1]], split[0])
+	}
+
+	pools := make(map[string]struct{})
+	for key, _ := range connections {
+		pools[key] = struct{}{}
+	}
+	old := make(map[string]struct{})
+
+	for hash(old) != hash(pools) {
+		for current, _ := range connections {
+			for pool, _ := range pools {
+				if containz(connections, pool, current) {
+					pool2 := sortKey(pool + "," + current)
+					delete(pools, pool)
+					pools[pool2] = struct{}{}
+				}
+			}
+		}
+		old = pools
+	}
+
+	//find the longest pool
+	longest := ""
+	for pool, _ := range pools {
+		if len(pool) > len(longest) {
+			longest = pool
+		}
+	}
+	return longest
+}
+
+func hash(m map[string]struct{}) int {
+	h := 0
+	for key, _ := range m {
+		h += len(key)
+	}
+	return h
 }
 
 func part1(input []string) int {
@@ -60,6 +107,26 @@ func contains(s []string, e string) bool {
 		}
 	}
 	return false
+}
+
+func getKeys(s string) []string {
+	return strings.Split(s, ",")
+}
+
+func sortKey(s string) string {
+	keys := getKeys(s)
+	sort.Strings(keys)
+	return strings.Join(keys, ",")
+}
+
+func containz(connections map[string][]string, s string, e string) bool {
+	for _, k := range getKeys(s) {
+		conns := connections[k]
+		if !contains(conns, e) {
+			return false
+		}
+	}
+	return true
 }
 
 type pair struct {
